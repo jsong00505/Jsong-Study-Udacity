64c8e1e241b1bb821f2a1be41a694f276caf5f49
jsong-pc <jsong00505@gmail.com>
2016-11-08 21:10:39 +0900

    refact: do some practices

---
+++ udacity-vr-nanodegree/practice3-1-jsong/Assets/RotateLight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateLight : MonoBehaviour {
+
+    public GameObject directionalLight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        directionalLight.transform.Rotate(0f, Time.deltaTime, 0f);
+
+        //Quaternion startRotation = Quaternion.Euler(50f, 30f, 0f);
+        //Quaternion endRoatation = startRotation * Quaternion.Euler(0, 180f, 0f);
+
+        //directionalLight.transform.rotation = Quaternion.Slerp(startRotation, endRoatation, Time.time);
+
+    }
+}
