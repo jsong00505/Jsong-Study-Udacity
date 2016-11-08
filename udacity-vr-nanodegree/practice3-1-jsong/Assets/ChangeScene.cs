dbb286fc33a1772e10b0035da022cecf390be7f6
jsong-pc <jsong00505@gmail.com>
2016-11-08 20:26:44 +0900

    refact: some changes to practice 3-3
    Maybe I do some practices in 3-3. So just make a little changes of codes

--- udacity-vr-nanodegree/practice3-1-jsong/Assets/ChangeScene.cs
+++ udacity-vr-nanodegree/practice3-1-jsong/Assets/ChangeScene.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
-    public void GoToScene()
+    public void GoToScene(string sceneName)
     {
         Debug.Log("My method was called!");
+        // SceneManager.LoadScene("00-FallingCoconut");
+        SceneManager.LoadScene(sceneName);
     }
 
 }
