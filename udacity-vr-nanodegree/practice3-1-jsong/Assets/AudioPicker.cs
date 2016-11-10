75bd2fd52c71b0d4b236a570ed72607aaa05ca9b
jsong-pc <jsong00505@gmail.com>
2016-11-10 22:24:50 +0900

    refact: do practice

---
+++ udacity-vr-nanodegree/practice3-1-jsong/Assets/AudioPicker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPicker : MonoBehaviour {
+
+    public AudioClip[] soundFiles;
+
+    public AudioSource soundSource;
+
+	// Use this for initialization
+	void Start () {
+        int index = Random.Range(0, soundFiles.Length);
+        soundSource.clip = soundFiles[index];
+        soundSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
