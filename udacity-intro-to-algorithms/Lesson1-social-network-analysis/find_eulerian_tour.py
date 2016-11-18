d447e36155ae5f078ce319959f39b21f3f266f6b
jsong-pc <jsong00505@gmail.com>
2016-11-18 23:45:52 +0900

    refact: solving #2

--- udacity-intro-to-algorithms/Lesson1-social-network-analysis/find_eulerian_tour.py
+++ udacity-intro-to-algorithms/Lesson1-social-network-analysis/find_eulerian_tour.py
@@ -11,29 +11,37 @@
 
 def find_eulerian_tour(graph):
     # your code here
-    return connected_nodes(graph)
+    degree = get_degree(graph)
+    print "degree --> ", degree
+    nodes = degree.keys()
+    if check_odd(nodes, degree):
+        connected = connected_nodes(graph)
+        print "connected --> ", connected
+    else:
+        return False
+
+    if connected.__len__() == nodes.__len__():
+        print "SAME"
+
 
 
 # These functions are required to find Eulerian Tour.
-def connected_nodes(tour):
-    """return the set of nodes reachable from
-    the first node in `tour`"""
-    a = tour[0][0]
-    print a
-    nodes = set([a])
-    print nodes
-    explore = set([a])
-    print explore
-    while len(explore) > 0:
-        # see what other nodes we can reach
-        b = explore.pop()
-        for t in tour:
-            node = check_edge(t, b, nodes)
-            if node is None:
-                continue
-            nodes.add(node)
-            explore.add(node)
-    return nodes
+def check_odd(nodes, degree):
+    for node in nodes:
+        try:
+            e = degree[node]
+            if e % 2 == 1:
+                return False
+        except:
+            return False
+    return True
+
+def get_degree(tour):
+    degree = {}
+    for x, y in tour:
+        degree[x] = degree.get(x, 0) + 1
+        degree[y] = degree.get(y, 0) + 1
+    return degree
 
 def check_edge(t, b, nodes):
     """
@@ -51,5 +59,42 @@ def check_edge(t, b, nodes):
         if t[0] not in nodes:
             return t[0]
     return None
+
+def connected_nodes(tour):
+    """return the set of nodes reachable from
+    the first node in `tour`"""
+    a = tour[0][0]
+    nodes = set([a])
+    explore = set([a])
+    while len(explore) > 0:
+        # see what other nodes we can reach
+        b = explore.pop()
+        for t in tour:
+            node = check_edge(t, b, nodes)
+            if node is None:
+                continue
+            nodes.add(node)
+            explore.add(node)
+    return nodes
+
+def is_eulerian_tour(nodes, tour):
+    # all nodes must be even degree
+    # and every node must be in graph
+    degree = get_degree(tour)
+    for node in nodes:
+        try:
+            d = degree[node]
+            if d % 2 == 1:
+                print "Node %s has odd degree" % node
+                return False
+        except KeyError:
+            print "Node %s was not in your tour" % node
+            return False
+    connected = connected_nodes(tour)
+    if len(connected) == len(nodes):
+        return True
+    else:
+        print "Your graph wasn't connected"
+        return False
 
 print find_eulerian_tour([(1, 2), (2, 3), (3, 1)])
