d3ff6da64c30164fb47b1c6c9eba632fc2fcd6c0
jsong-pc <jsong00505@gmail.com>
2016-12-07 21:57:37 +0900

    fear: animate the key moved
    The key moved up and down, and it will be rotated in soon.
    
    Solved: #3

--- udacity-vr-nanodegree/project-3-jsong/Assets/Udacity/Scripts/Key.cs
+++ udacity-vr-nanodegree/project-3-jsong/Assets/Udacity/Scripts/Key.cs
@@ -8,12 +8,19 @@ public class Key : MonoBehaviour
     //Create a reference to the KeyPoofPrefab and Door
     public GameObject KeyPoofPrefab;
     public Door door;
-
+    int directionFlag = 2;
 	void Update()
 	{
         // Bonus: Key Animation
         // Make the object go up and down or rotate
-
+        if (gameObject.transform.position.y > 5)
+        {
+            directionFlag = -2;
+        } else if (gameObject.transform.position.y < 3)
+        {
+            directionFlag = 2;
+        }
+        gameObject.transform.Translate(0, directionFlag * Time.deltaTime, 0, Space.World);
 
     }
 
