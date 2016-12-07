80f50928c2f18db8fe75840a0c0fa913da661351
jsong-pc <jsong00505@gmail.com>
2016-12-07 21:56:27 +0900

    fear: animate the door opened(cont.)
    Need to open when it clicked automatically, but now it open when the key collected. So need to change.
    
    Solved: #3

--- udacity-vr-nanodegree/project-3-jsong/Assets/Udacity/Scripts/Door.cs
+++ udacity-vr-nanodegree/project-3-jsong/Assets/Udacity/Scripts/Door.cs
@@ -6,20 +6,28 @@ public class Door : MonoBehaviour
 {
     // Create a boolean value called "locked" that can be checked in Update() 
     bool locked = true;
+
     void Update() {
         // If the door is unlocked and it is not fully raised
         if(!locked)
         {
             // Animate the door raising up
-            locked = true;
-
+            gameObject.transform.Translate(0, 2*Time.deltaTime, 0, Space.World);
+            if(gameObject.transform.position.y > 25)
+            {
+                locked = true;
+                Debug.Log("The door is opened comletely");
+            } else
+            {
+                Debug.Log("Running" + Time.deltaTime);
+            }
+            
         }
     }
 
     public void Unlock()
     {
         // You'll need to set "locked" to true here
-        
         locked = false;
     }
 }
