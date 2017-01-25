d253d578161b99b98f058b47880041479afffcc7
jsong-pc <jsong00505@gmail.com>
2017-01-25 22:45:40 +0900

    fix: connect postgreSQL

--- intro-to-relational-databases/fsnd-virtual-machine/vagrant/forum/forumdb.py
+++ intro-to-relational-databases/fsnd-virtual-machine/vagrant/forum/forumdb.py
@@ -1,11 +1,11 @@
 #
 # Database access functions for the web forum.
-# 
+#
 
-import time
+import psycopg2
 
 ## Database connection
-DB = []
+
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +16,12 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    query = "select content, time from posts order by time desc"
+    c.execute(query)
+    posts = ({'content':str(row[1]), 'time':str(row[0])} for row in c.fetchall())
+    DB.close()
     return posts
 
 ## Add a post to the database.
@@ -27,5 +31,9 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    query = "insert into posts values('%s')" % content
+    c.execute(query)
+    DB.commit()
+    DB.close()
